62534dae6dbfdd7fe9596e126ee81c84d670951c
Khann777 <64934869+Khann777@users.noreply.github.com>
2022-08-28 15:32:00 +0600

    Changed to GitHub

---
+++ HelloWorld/HelloWorld/SecondTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class SecondTask
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, ");
+            Console.Write("World ");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice2.7/Practice2.7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Alex Smith";
+            byte age = 17;
+            string email = "alexs17@gmail.com";
+            double programmingScore = 78.8;
+            double mathScore = 90;
+            double physicsScore = 60.3;
+            double totalScore = programmingScore + mathScore + physicsScore;
+            double medianScore = totalScore / 3;
+
+            Console.WriteLine(
+                $"Name: {fullName}\n" +
+                $"Age: {age}\n" +
+                $"E-mail: {email}\n" +
+                $"Programming score: {programmingScore}\n" +
+                $"Math score: {mathScore}\n" +
+                $"Physics score: {physicsScore}");
+            Console.ReadKey();
+
+            Console.WriteLine(
+                $"Total score: {totalScore}\n" +
+                $"Median score: {medianScore}");
+            Console.ReadKey();
+
+        }
+    }
+}

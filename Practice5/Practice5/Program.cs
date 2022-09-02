a92f054e7da0155a6725206fdb5082fbae28899a
Khann777 <64934869+Khann777@users.noreply.github.com>
2022-09-02 17:54:31 +0600

    Updated Practice 5

--- Practice5/Practice5/Program.cs
+++ Practice5/Practice5/Program.cs
@@ -15,21 +15,45 @@ namespace Practice5
             Console.ReadKey();
         }
 
+        #region Split and Reverse methods
+
+        /// <summary>
+        /// Reverses string array
+        /// </summary>
+        /// <param name="sentences">String array to reverse</param>
         static void ReverceSentence(string[] sentences)
         {
             Array.Reverse(sentences);
-            Print(sentences);
+            string newString = string.Join(" ", sentences);
+            
+            Print(newString);           // For Reversed string array, prints sentence
         }
 
+        /// <summary>
+        /// Splits string into words and adds to array
+        /// </summary>
+        /// <param name="sentence">string to be splitted</param>
         static void SplitSentence(string sentence)
         {
             string[] sentences = sentence.Split(' ');
+            Print(sentences);       // For Splited words, prints words
             ReverceSentence(sentences);
         }
+        #endregion
 
+        #region Print methods
         static void Print(string[] sentence)
         {
-            foreach (string s in sentence) { Console.WriteLine(s); }
+            foreach (string s in sentence)
+            { 
+                Console.WriteLine(s); 
+            }
         }
+
+        static void Print(string sentence) 
+        {
+            Console.WriteLine(sentence);
+        }
+        #endregion
     }
 }

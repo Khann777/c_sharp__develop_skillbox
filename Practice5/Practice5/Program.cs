e936cc71794946b09e7f8d1fb93e77d06683a51b
Khann777 <64934869+Khann777@users.noreply.github.com>
2022-09-02 17:40:14 +0600

    Completed practice 5

---
+++ Practice5/Practice5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            SplitSentence(sentence);
+            Console.ReadKey();
+        }
+
+        static void ReverceSentence(string[] sentences)
+        {
+            Array.Reverse(sentences);
+            Print(sentences);
+        }
+
+        static void SplitSentence(string sentence)
+        {
+            string[] sentences = sentence.Split(' ');
+            ReverceSentence(sentences);
+        }
+
+        static void Print(string[] sentence)
+        {
+            foreach (string s in sentence) { Console.WriteLine(s); }
+        }
+    }
+}

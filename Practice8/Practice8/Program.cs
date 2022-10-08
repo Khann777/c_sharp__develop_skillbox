76dd1725f73eedc0367a570252393e4eb431baf4
Khann777 <nurdinov.d777@gmail.com>
2022-10-09 00:52:35 +0600

    Practice 8 part 1 completed

---
+++ Practice8/Practice8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> wholeNumbers = new List<int>();
+
+            int i = 0;
+            while (i < 100)
+            {
+                wholeNumbers.Add(random.Next(100));
+                i++;
+            }
+
+            PrintNumbers(wholeNumbers);
+            Console.ReadKey();
+
+            DeleteFromList(wholeNumbers);
+            PrintNumbers(wholeNumbers);
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Prints all numbers in list and next line prints count
+        /// </summary>
+        /// <param name="list">Collection to print</param>
+        public static void PrintNumbers(List<int> list)
+        {
+            foreach (int num in list)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine($"\nList count: {list.Count}");
+        }
+
+        /// <summary>
+        /// Deletes numbers from list than are more than 25 and less than 50
+        /// </summary>
+        /// <param name="list">Collection to delete range</param>
+        public static void DeleteFromList(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int num = list[i];
+                list.RemoveAll(num => num > 25 && num < 50);
+            }
+        }
+    }
+}

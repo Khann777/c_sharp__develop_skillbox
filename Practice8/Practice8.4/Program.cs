e560ea5ba3198a44495942fe7b1e8c52e4f4c8bd
Khann777 <nurdinov.d777@gmail.com>
2022-10-14 12:53:21 +0600

    Completed Practice 8 part 4

---
+++ Practice8/Practice8.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Practice8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement xmlTree1 =
+                new XElement("Address",
+                    new XElement("Street", "Street Name"),
+                    new XElement("HouseNumber", "House Number"),
+                    new XElement("FlatNumber", "Flat Number")
+                    );
+
+            XElement xmlTree2 =
+                new XElement("Phones",
+                    new XElement("MobilePhone", "89999999999999"),
+                    new XElement("FlatPhone", "123-45-67")
+                    );
+
+            XElement xmlName = new XElement("Person",
+                new XAttribute("name", "Full Name"),
+                xmlTree1,
+                xmlTree2
+                );
+            Console.WriteLine(xmlName);
+            Console.ReadKey();
+        }
+    }
+}

97c2fc1879ca40359aefb2f00ae2fdad94a06f18
Davlyat Nurdinov <nurdinov.d777@gmail.com>
2022-11-28 11:11:49 +0600

    Practice 11 start up

---
+++ Practice11/ClientData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice11
+{
+    internal class ClientData
+    {
+        /// <summary>
+        /// Clients last name
+        /// </summary>
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// Clients first name
+        /// </summary>
+        public string FirstName { get; private set; }
+
+        /// <summary>
+        /// Clients fathers name or second name
+        /// </summary>
+        public string FathersName { get; private set; }
+
+        /// <summary>
+        /// Clients phone number
+        /// </summary>
+        public uint PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Clients passport series
+        /// </summary>
+        public uint PassportSeries { get; set; }
+
+        /// <summary>
+        /// Clients passport number
+        /// </summary>
+        public uint PassportNumber { get; set; }
+
+        public virtual void ViewInformation()
+        {
+
+        }
+    }
+}
